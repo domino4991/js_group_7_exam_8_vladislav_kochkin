6163a5b357eb214f7653ef2d19335a8b0b60d3a5
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-29 14:12:56 +0600

    Убрал из констант позицию All

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 export const CATEGORY_NAMES = [
-    {name: 'All', category: 'all'},
     {name: 'Star Wars', category: 'star-wars'},
     {name: 'Famous people', category: 'famouse-people'},
     {name: 'Saying', category: 'saying'},
