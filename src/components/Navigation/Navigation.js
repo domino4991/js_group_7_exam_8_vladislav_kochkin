481807b036bf2faa2d4aaa84653f30ac526c4dc0
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-29 12:26:58 +0600

    Переделал структуру навигации

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,19 +4,17 @@ import {CATEGORY_NAMES} from "../../constants";
 import {NavLink} from "react-router-dom";
 
 const Navigation = () => {
-    let categoryList = CATEGORY_NAMES.map(item => (
-        <li className="Category__list-item">
-            <NavLink
-                path={`/quotes/${item.category}`}
-                className="Category__list-link"
-                activeClassName="Category__list-link_active"
-            />
-        </li>
-    ));
-
     return (
         <ul className="Category__list">
-            {categoryList}
+            {CATEGORY_NAMES.map(item => (
+                <li className="Category__list-item">
+                    <NavLink
+                        to={`/quotes/${item.category}`}
+                        className="Category__list-link"
+                        activeClassName="Category__list-link_active"
+                    > {item.name}</NavLink>
+                </li>
+            ))}
         </ul>
     );
 };
