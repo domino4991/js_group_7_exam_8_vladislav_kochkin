23a808545520332636684abafd1c6ff0b322f5db
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-29 13:56:21 +0600

    Создал компонент шапки

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 
 import {ImQuotesLeft} from 'react-icons/im';
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
@@ -13,8 +14,24 @@ const Header = () => {
                 </div>
                 <nav className="Header__nav">
                     <ul className="Header__nav-list">
-                        <li className="Header__nav-item">Quotes</li>
-                        <li className="Header__nav-item">Add new quotes</li>
+                        <li className="Header__nav-item">
+                            <NavLink
+                                to="/quotes/all"
+                                className="Header__nav-link"
+                                activeClassName="Header__nav-link_active"
+                            >
+                                Quotes
+                            </NavLink>
+                        </li>
+                        <li className="Header__nav-item">
+                            <NavLink
+                                to="/add-quote"
+                                className="Header__nav-link"
+                                activeClassName="Header__nav-link_active"
+                            >
+                                Add new quotes
+                            </NavLink>
+                        </li>
                     </ul>
                 </nav>
             </div>
