54f060c49d9f5b72851bbd0fde921cf64b14f9ee
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-29 14:13:35 +0600

    Изменил навигацию по категориям

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,6 +6,13 @@ import {NavLink} from "react-router-dom";
 const Navigation = () => {
     return (
         <ul className="Category__list">
+            <li className="Category__list-item">
+                <NavLink
+                    to="/"
+                    className="Category__list-link"
+                    activeClassName="Category__list-link_active"
+                >All</NavLink>
+            </li>
             {CATEGORY_NAMES.map((item, i) => (
                 <li key={i} className="Category__list-item">
                     <NavLink
