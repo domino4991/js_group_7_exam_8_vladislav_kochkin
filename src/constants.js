11543579aec92800e668cba9831a347cb228759a
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-29 15:56:58 +0600

    Переименовал Famouse people на Famous people

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const CATEGORY_NAMES = [
     {name: 'Star Wars', category: 'star-wars'},
-    {name: 'Famous people', category: 'famouse-people'},
+    {name: 'Famous people', category: 'famous-people'},
     {name: 'Saying', category: 'saying'},
     {name: 'Humour', category: 'humour'},
     {name: 'Motivational', category: 'motivational'}
